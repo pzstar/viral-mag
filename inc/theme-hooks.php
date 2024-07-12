3f616ea63691e194f210d6abc8b511004597aad5
Prabin Dangol <neprabin@gmail.com>
2024-07-12 17:19:05 +0545

    Update theme-hooks.php

--- inc/theme-hooks.php
+++ inc/theme-hooks.php
@@ -217,6 +217,20 @@ add_filter('viral_mag_regsiter_fonts', 'viral_mag_add_custom_fonts');
 
 function viral_mag_premium_demo_config($demos) {
     $premium_demos = array(
+        'newspaper' => array(
+            'name' => 'Viral Pro - NewsPaper',
+            'type' => 'pro',
+            'buy_url' => 'https://hashthemes.com/wordpress-theme/viral-pro/',
+            'image' => 'https://hashthemes.com/import-files/viral-pro/screen/newspaper.jpg',
+            'preview_url' => 'https://demo.hashthemes.com/viral-pro/newspaper/',
+            'tags' => array(
+                'premium' => 'Premium'
+            ),
+            'pagebuilder' => array(
+                'customizer' => 'Customizer',
+                'elementor' => 'Elementor'
+            )
+        ),
         'magazine' => array(
             'name' => 'Viral Pro - Magazine',
             'type' => 'pro',
